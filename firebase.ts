62f6def8b4ef412638b446172ad3176b99d7cdfe
harislakhani56 <harislakhani516@gmail.com>
2026-02-12 13:29:19 +0500

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -7,8 +7,7 @@ const firebaseConfig = {
   projectId: "lohar-wadha-cricket",
   storageBucket: "lohar-wadha-cricket.firebasestorage.app",
   messagingSenderId: "415966561150",
-  appId: "1:415966561150:web:8109570052ab1ded40e92f",
-  measurementId: "G-Z2S6YL8L9S"
+  appId: "1:415966561150:web:8109570052ab1ded40e92f"
 };
 
 const app = initializeApp(firebaseConfig);
